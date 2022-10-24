d925b32dd7701727871b34fd850b829952e12bff
areel321 <112980267+areel321@users.noreply.github.com>
2022-10-24 17:10:02 -0400

    commit client.py

--- client.py
+++ client.py
@@ -1,3 +1,4 @@
+"""
 import http.client
 import sys
 
@@ -15,3 +16,22 @@ response = conn.getresponse()
 data = response.read()
 
 #client(server_host, server_port, filename)
+"""
+
+from socket import *
+import sys
+#server_host = sys.argv[1:]
+#server_port = sys.argv[2]
+#filename = sys.argv[3]
+server_host = '131.229.198.171'
+server_port = 1024
+filename = 'HelloWorld.html'
+
+def client(server_host, server_port, filename):
+    client_socket = socket(AF_INET, SOCK_STREAM)
+    client_socket.connect((str(server_host), server_port))
+    client_socket.send(filename.encode())
+    client_socket.recv(server_port).decode()
+    client_socket.close()
+
+client(server_host, server_port, filename)
