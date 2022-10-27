9b20df1e163ac1eaeddaae2d64cade84c463d74e
areel321 <112980267+areel321@users.noreply.github.com>
2022-10-27 15:11:06 -0400

    commit client commit reflection

--- client.py
+++ client.py
@@ -1,23 +1,22 @@
 
-# i consulted the python HTTP documentation on how to create a client-side of a server here https://docs.python.org/3/library/http.client.html
-import http.client
+from socket import *
 import sys
-
 
 #server_host = sys.argv[1:16]
 #server_port = sys.argv[17:20]
 #filename = sys.argv[21:]
-server_host = ''
+server_host = '131.229.198.171'
 server_port = 1024
-filename = 'HelloWorld.html'
+filename = b'/HelloWorld.html'
 
-#add / to the beginning of the filename
-filename = str('/'+filename)
-#start the HTTP connection
-conn = http.client.HTTPConnection(str(server_host), server_port)
-#use get request
-conn.request("GET", filename)
-#recieve and read contents of the server
-response = conn.getresponse()
-data = response.read()
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((server_host, server_port))
+
+try:
+    clientSocket.sendall(filename)
+    data = clientSocket.recv(4096)
+    print('recieved')
+
+except IOError:
+    print("error")
 
