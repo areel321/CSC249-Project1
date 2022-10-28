b62f9ed5abccd9d0eff6b3a6cb85a0e588f47f64
areel321 <112980267+areel321@users.noreply.github.com>
2022-10-28 00:59:24 -0400

    commit client

--- client.py
+++ client.py
@@ -2,19 +2,21 @@
 from socket import *
 import sys
 
-#server_host = sys.argv[1:16]
-#server_port = sys.argv[17:20]
-#filename = sys.argv[21:]
+#server_host = sys.argv[1]
+#server_port = sys.argv[2]
+#filename = sys.argv[3]
 server_host = '131.229.198.171'
 server_port = 1024
-filename = b'/HelloWorld.html'
+filename = 'HelloWorld.html'
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((server_host, server_port))
 
 try:
-    clientSocket.sendall(filename)
-    data = clientSocket.recv(4096)
+    clientSocket.sendall(f'GET /{filename}\nContent-Type: text/html\n\n'.encode())
+    message = clientSocket.recv(4096)
+    data = message.decode()
+
     print('recieved')
 
 except IOError:
