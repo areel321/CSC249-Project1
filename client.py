2e64704dc264067319c396db5dc0a9ed4f27cabe
areel321 <112980267+areel321@users.noreply.github.com>
2022-10-31 13:35:21 -0400

    commit client

--- client.py
+++ client.py
@@ -1,24 +1,31 @@
-
+#import socket
 from socket import *
+#import sys to accept command line arguments
 import sys
 
+#fill variables from command line
 #server_host = sys.argv[1]
 #server_port = sys.argv[2]
 #filename = sys.argv[3]
+
+#hardcoded variables
 server_host = '131.229.198.171'
 server_port = 1024
 filename = 'HelloWorld.html'
 
+#set up client socket and connect to server
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((server_host, server_port))
 
 try:
+    #send HTML file to socket
     clientSocket.sendall(f'GET /{filename}\nContent-Type: text/html\n\n'.encode())
     message = clientSocket.recv(4096)
     data = message.decode()
 
     print('recieved')
 
+#handle errors
 except IOError:
     print('error')
 
